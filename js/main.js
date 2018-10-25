3c6f998c0dced0570b770d1419b81e83e71c511a
Maydya <onepiece762@gmail.com>
2018-10-26 02:02:46 +0800

    V0.7
    Add animate and fixed the css

--- js/main.js
+++ js/main.js
@@ -1,8 +1,14 @@
 $(function () {
 
+	var wow = new WOW({
+		offset: 50
+	});
+
+	wow.init();
+
 	$('.progress-bar').waypoint(function () {
 		$('.progress-bar').css({
-			animation: 'progress 1s forwards'
+			animation: 'progress 1.5s forwards'
 		}).find('span').fadeIn(1300);
 	},{offset: '75%'});
 	
